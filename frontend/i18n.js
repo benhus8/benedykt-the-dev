c4bbe6ea5cb467eade705ed6537f19959dded2d9
benhus8 <benedykt.huszcza@gmail.com>
2024-12-19 22:29:14 +0100

    fix deprecated param

--- frontend/i18n.js
+++ frontend/i18n.js
@@ -1,13 +1,11 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-// Can be imported from a shared config
 const locales = ['en', 'pl'];
 
-export default getRequestConfig(async ({ locale }) => {
-    // Validate that the incoming `locale` parameter is valid
+export default getRequestConfig(async ({ requestLocale }) => {
+    const locale = await requestLocale;
     if (!locales.includes(locale)) notFound();
-
     return {
         messages: (await import(`./locales/${locale}.json`)).default,
     };
