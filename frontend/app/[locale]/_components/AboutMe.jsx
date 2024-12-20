e7bbc3b2b77c80ea2f2f7eab49af3b4c59278c04
benhus8 <benedykt.huszcza@gmail.com>
2024-12-20 22:57:44 +0100

    add animation and stack icons

--- frontend/app/[locale]/_components/AboutMe.jsx
+++ frontend/app/[locale]/_components/AboutMe.jsx
@@ -1,6 +1,22 @@
 "use client";
 
 import {Typewriter} from "react-simple-typewriter";
+import AnimatedIcon from "@/components/animated/AnimatedIcons";
+
+
+const icons = [
+    { src: "/stack/fast-api.png", alt: "FastAPI", initialX: 150, initialY: 150, endX: 0, endY: 40 },
+    { src: "/stack/java.png", alt: "Java", initialX: 150, initialY: 150, endX: 50, endY: 90 },
+    { src: "/stack/js.png", alt: "JavaScript", initialX: 150, initialY: 150, endX: 225, endY: 25 },
+    { src: "/stack/postgres.png", alt: "PostgreSQL", initialX: 150, initialY: 150, endX: 300, endY:75 },
+    { src: "/stack/python.png", alt: "Python", initialX: 150, initialY: 150, endX: -20, endY: 125 },
+    { src: "/stack/next.png", alt: "Next", initialX: 150, initialY: 150, endX: -30, endY: 200 },
+    { src: "/stack/rabbitmq.png", alt: "RabbitMQ", initialX: 150, initialY: 150, endX: 100, endY: 10 },
+    { src: "/stack/react.png", alt: "React", initialX: 150, initialY: 150, endX: 320, endY: 175 },
+    { src: "/stack/spring.png", alt: "Spring", initialX: 150, initialY: 150, endX: 250, endY: 130 },
+];
+
+
 
 export default function IntroSection() {
     return (
@@ -40,6 +56,18 @@ export default function IntroSection() {
                         alt="Benedykt"
                         className="scale-75"
                     />
+                    {icons.map((icon, index) => (
+                        <AnimatedIcon
+                            key={index}
+                            src={icon.src}
+                            alt={icon.alt}
+                            initialX={icon.initialX}
+                            initialY={icon.initialY}
+                            endX={icon.endX}
+                            endY={icon.endY}
+                            delay={index * 0.3}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
