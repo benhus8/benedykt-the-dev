d4597072031ac5b0ed391c561bf5f0507fcb7bd1
benhus8 <benedykt.huszcza@gmail.com>
2025-01-14 22:27:26 +0100

    code cleanup

--- frontend/app/[locale]/page.jsx
+++ frontend/app/[locale]/page.jsx
@@ -1,5 +1,4 @@
 import Transition from "@/components/animated/Transition";
-import { useTranslations } from "next-intl";
 import { useTranslations } from 'next-intl';
 import Me from "./_components/Me";
 import StackSection from "@/app/[locale]/_components/Stack";
@@ -8,15 +7,15 @@ export default function Home() {
   const t = useTranslations("Home.Hero");
 
   return (
-    <Transition>
-      <section>
-        <div className="px-20 pt-2">
-          <Me />
-        </div>
-      </section>
-      <section id="stack">
-        <StackSection />
-      </section>
-    </Transition>
+      <Transition>
+          <section>
+              <div className="px-20 pt-2">
+                  <Me/>
+              </div>
+          </section>
+          <section id="stack">
+              <StackSection/>
+          </section>
+      </Transition>
   );
 }
