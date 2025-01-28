e3d25efe8c626e2e66e6a5feaf4e23ad8e31eded
benhus8 <benedykt.huszcza@gmail.com>
2025-01-28 22:57:38 +0100

    extend page with my skills section

--- frontend/app/[locale]/page.jsx
+++ frontend/app/[locale]/page.jsx
@@ -2,6 +2,7 @@ import Transition from "@/components/animated/Transition";
 import { useTranslations } from 'next-intl';
 import Me from "./_components/Me";
 import StackSection from "@/app/[locale]/_components/Stack";
+import {MySkills} from "@/app/[locale]/_components/MySkills";
 
 export default function Home() {
   const t = useTranslations("Home.Hero");
@@ -13,9 +14,13 @@ export default function Home() {
                   <Me/>
               </div>
           </section>
+          <section>
+              <MySkills/>
+          </section>
           <section id="stack">
               <StackSection/>
           </section>
+
       </Transition>
   );
 }
