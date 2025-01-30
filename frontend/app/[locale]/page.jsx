bedb9dcbd07bc63652ff828b505756ff89289ea0
benhus8 <benedykt.huszcza@gmail.com>
2025-01-30 23:14:40 +0100

    extend page with projects section

--- frontend/app/[locale]/page.jsx
+++ frontend/app/[locale]/page.jsx
@@ -1,26 +1,29 @@
 import Transition from "@/components/animated/Transition";
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 import Me from "./_components/Me";
 import StackSection from "@/app/[locale]/_components/Stack";
-import {MySkills} from "@/app/[locale]/_components/MySkills";
+import { MySkills } from "@/app/[locale]/_components/MySkills";
+import { MyProjects } from "@/app/[locale]/_components/MyProjects";
 
 export default function Home() {
   const t = useTranslations("Home.Hero");
 
   return (
-      <Transition>
-          <section>
-              <div className="px-20 pt-2">
-                  <Me/>
-              </div>
-          </section>
-          <section>
-              <MySkills/>
-          </section>
-          <section id="stack">
-              <StackSection/>
-          </section>
-
-      </Transition>
+    <Transition>
+      <section>
+        <div className="px-20 pt-2">
+          <Me />
+        </div>
+      </section>
+      <section>
+        <MySkills />
+      </section>
+      <section>
+        <MyProjects />
+      </section>
+      <section id="stack">
+        <StackSection />
+      </section>
+    </Transition>
   );
 }
