b8566436a8bfc81ced058f75a26c79c0dc4ce09d
benhus8 <benedykt.huszcza@gmail.com>
2025-02-20 09:42:12 +0100

    extend page

--- frontend/app/[locale]/page.jsx
+++ frontend/app/[locale]/page.jsx
@@ -5,29 +5,33 @@ import StackSection from "@/app/[locale]/_components/Stack";
 import { MySkills } from "@/app/[locale]/_components/MySkills";
 import { MyProjects } from "@/app/[locale]/_components/MyProjects";
 import { Career } from "@/app/[locale]/_components/Career";
+import { Contact } from "@/app/[locale]/_components/Contact";
 
 export default function Home() {
   const t = useTranslations("Home.Hero");
 
   return (
-    <Transition>
-      <section>
-        <div className="px-20 pt-2 ">
-          <Me />
-        </div>
-      </section>
-      <section>
-        <MySkills />
-      </section>
-      <section>
-        <MyProjects />
-      </section>
-      <section>
-        <Career />
-      </section>
-      <section id="stack">
-        <StackSection />
-      </section>
-    </Transition>
+      <Transition>
+          <section>
+              <div className="px-20 pt-2 ">
+                  <Me/>
+              </div>
+          </section>
+          <section>
+              <MySkills/>
+          </section>
+          <section>
+              <MyProjects/>
+          </section>
+          <section>
+              <Career/>
+          </section>
+          <section>
+              <Contact/>
+          </section>
+          <section id="stack">
+              <StackSection/>
+          </section>
+      </Transition>
   );
 }
