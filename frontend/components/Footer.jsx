96102385d03dd8ee152ad8141c001bbfc477d7d6
benhus8 <benedykt.huszcza@gmail.com>
2025-02-21 23:11:13 +0100

    add lang for footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+
 const FooterItem = ({ text, targetId, bold, toTop, link }) => {
   const handleClick = () => {
     if (link) {
@@ -29,6 +31,8 @@ const FooterItem = ({ text, targetId, bold, toTop, link }) => {
 };
 
 export default function Footer() {
+  const t = useTranslations("footer");
+
   return (
     <footer className="flex flex-col items-center justify-center bg-black text-white text-center py-6 px-4">
       <div className="flex flex-row items-center justify-center gap-6 mt-4">
@@ -39,7 +43,7 @@ export default function Footer() {
         >
           <img
             src="/github.svg"
-            alt="GitHub"
+            alt={t("social_media.github")}
             className="w-[24px] h-[24px] mt-2 transition-transform duration-300 hover:scale-110"
           />
         </a>
@@ -50,24 +54,24 @@ export default function Footer() {
         >
           <img
             src="/linkdin.svg"
-            alt="LinkedIn"
+            alt={t("social_media.linkedin")}
             className="w-[24px] h-[24px] mt-2 transition-transform duration-300 hover:scale-110"
           />
         </a>
       </div>
 
-      <p className="text-lg font-semiBold mt-4">{"benedykt.huszcza.dev"}</p>
+      <p className="text-lg font-semiBold mt-4">{t("brand")}</p>
 
       <div className="w-full flex flex-col sm:flex-row items-center justify-center mt-4 gap-4 sm:gap-8 text-lg">
-        <FooterItem text={"Skills"} targetId={"skills"} />
-        <FooterItem text={"Projects"} targetId={"projects"} />
-        <FooterItem text={"Career"} targetId={"career"} />
-        <FooterItem text={"Contact"} targetId={"contact"} />
-        <FooterItem text={"Blog"} link="https://blog.huszcza.dev" />
+        <FooterItem text={t("menu.skills")} targetId={"skills"} />
+        <FooterItem text={t("menu.projects")} targetId={"projects"} />
+        <FooterItem text={t("menu.career")} targetId={"career"} />
+        <FooterItem text={t("menu.contact")} targetId={"contact"} />
+        <FooterItem text={t("menu.blog")} link="https://blog.huszcza.dev" />
       </div>
 
       <p className="text-sm font-light mt-4">
-        {`Designed and created by Benedytkt Huszcza @${new Date().getFullYear()}`}
+        {`${t("credits")}${new Date().getFullYear()}`}
       </p>
     </footer>
   );
